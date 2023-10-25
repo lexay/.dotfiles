30120a0c434f92211abe6bbaedc63c2f73419388
Alexey K <alexcrpts@gmail.com>
2023-10-25 17:17:51 +0300

    add mason

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -4,9 +4,14 @@ local M = {
 
 M.branch = "v3.x"
 M.dependencies = {
+  -- LSP support
+  "williamboman/mason.nvim",
+  "williamboman/mason-lspconfig.nvim",
   "neovim/nvim-lspconfig",
+  -- Autocompletion
   "hrsh7th/nvim-cmp",
   "hrsh7th/cmp-nvim-lsp",
+  -- Snippets
   "L3MON4D3/LuaSnip"
   }
 
