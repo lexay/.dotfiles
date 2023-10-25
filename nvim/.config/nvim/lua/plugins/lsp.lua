cb31988fa9bc6fbe55373755b8c018736af6fe19
Alexey K <alexcrpts@gmail.com>
2023-10-25 14:08:36 +0300

    add lsp-zero and base plugins required by it

---
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,13 @@
+local M = {
+  "VonHeikemen/lsp-zero.nvim"
+}
+
+M.branch = "v3.x"
+M.dependencies = {
+  "neovim/nvim-lspconfig",
+  "hrsh7th/nvim-cmp",
+  "hrsh7th/cmp-nvim-lsp",
+  "L3MON4D3/LuaSnip"
+  }
+
+return M
