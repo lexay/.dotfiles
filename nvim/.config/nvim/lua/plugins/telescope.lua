46013751031134d1f980a6689892a64111d27cfd
Alexey K <alexcrpts@gmail.com>
2023-10-26 12:10:10 +0300

    add keymap for finding man pages

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -34,6 +34,7 @@ M.config = function()
   keymap('n', '<leader>fg', ':Telescope live_grep<cr>')
   keymap('n', '<leader>fb', ':Telescope buffers<cr>')
   keymap('n', '<leader>fh', ':Telescope help_tags<cr>')
+  keymap('n', '<leader>fm', ':Telescope man_pages<cr>')
 end
 
 M.dependencies = {'nvim-lua/plenary.nvim'}
