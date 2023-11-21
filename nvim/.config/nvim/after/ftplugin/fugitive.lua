63618064daf61c16c63102c8e7ed9e2ce8ea2cb9
Alexey K <alexcrpts@gmail.com>
2023-11-22 00:01:26 +0300

    disable keymaps used for resizing in `fugitive`

--- nvim/.config/nvim/after/ftplugin/fugitive.lua
+++ nvim/.config/nvim/after/ftplugin/fugitive.lua
@@ -1,2 +1,8 @@
 -- Disable netrw
 vim.api.nvim_buf_set_keymap(0, "n", "<leader>dd", "<nop>", {})
+
+-- Disable keymaps used for resizing
+vim.api.nvim_buf_set_keymap(0, "n", "=", "nop", {})
+vim.api.nvim_buf_set_keymap(0, "n", "-", "nop", {})
+vim.api.nvim_buf_set_keymap(0, "n", "_", "nop", {})
+vim.api.nvim_buf_set_keymap(0, "n", "+", "nop", {})
