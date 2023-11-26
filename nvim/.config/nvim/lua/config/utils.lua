524813f92ed26cbc5f97472354d9974dfac144bd
Alexey K <alexcrpts@gmail.com>
2023-11-26 23:10:45 +0300

    make `buff_only` return when current buffer is only one left
    * Also print messages.

--- nvim/.config/nvim/lua/config/utils.lua
+++ nvim/.config/nvim/lua/config/utils.lua
@@ -22,11 +22,19 @@ end
 M.buff_only = function()
   local all_buffs = vim.api.nvim_list_bufs()
   local current_buff = vim.api.nvim_get_current_buf()
+  local buffs_count = table.getn(all_buffs)
+
+  if buffs_count == 1 then
+    print("Already only buffer")
+    return
+  end
+
   for _, i in ipairs(all_buffs) do
     if i ~= current_buff then
       vim.api.nvim_buf_delete(i, {})
     end
   end
+  print(buffs_count - 1 .. " " .. "buffers deleted")
 end
 
 return M
