a0ebad8e7ae0b81d28ec209dd86a26976e9b7695
Alexey K <alexcrpts@gmail.com>
2023-12-06 15:21:29 +0300

    fix `nop`

--- nvim/.config/nvim/after/ftplugin/fugitive.lua
+++ nvim/.config/nvim/after/ftplugin/fugitive.lua
@@ -2,7 +2,7 @@
 vim.api.nvim_buf_set_keymap(0, "n", "<leader>dd", "<nop>", {})
 
 -- Disable keymaps used for resizing
-vim.api.nvim_buf_set_keymap(0, "n", "=", "nop", {})
-vim.api.nvim_buf_set_keymap(0, "n", "-", "nop", {})
-vim.api.nvim_buf_set_keymap(0, "n", "_", "nop", {})
-vim.api.nvim_buf_set_keymap(0, "n", "+", "nop", {})
+vim.api.nvim_buf_set_keymap(0, "n", "=", "<nop>", {})
+vim.api.nvim_buf_set_keymap(0, "n", "-", "<nop>", {})
+vim.api.nvim_buf_set_keymap(0, "n", "_", "<nop>", {})
+vim.api.nvim_buf_set_keymap(0, "n", "+", "<nop>", {})
