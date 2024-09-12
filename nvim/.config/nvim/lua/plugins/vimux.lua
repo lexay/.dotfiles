e9be850a61f4053d22a03cebf674d82233b27bf5
Alexey K <alexcrpts@gmail.com>
2024-09-12 18:40:01 +0300

    (neovim): refactor `vimux`
    * Additional interpreters (commands) will follow eventually. So we need
      to be ready for that by switching to a key/value table rather than
      using if/else control statement.

--- nvim/.config/nvim/lua/plugins/vimux.lua
+++ nvim/.config/nvim/lua/plugins/vimux.lua
@@ -5,34 +5,34 @@ local M = {
 M.branch = "master"
 
 M.config = function()
-  local check_intpr = function()
-    local intpr
+  local check_cmd = function()
     local filetype = vim.bo.filetype
-    if filetype == "javascript" then
-      intpr = "node"
-    elseif filetype == "" then
-      intpr = "cat"
-    else
-      intpr = filetype
+
+    if filetype == "" then
+      return "cat"
     end
-    return intpr
+
+    local commands = {
+      javascript = "node",
+    }
+
+    return commands[filetype] or filetype
   end
 
-  function vimux_run(intpr)
-    intpr = intpr or check_intpr()
+  function run_cur_buf(cmd)
+    cmd = cmd or check_cmd()
     local bufname = vim.api.nvim_buf_get_name(0)
-    local command = string.format(" clear; %s %s", intpr, bufname)
-    vim.fn.VimuxRunCommand(command)
+    vim.fn.VimuxRunCommand(string.format(" clear; %s %s", cmd, bufname))
   end
 
   -- Keymaps
-  keymap("n", "<leader>vr", "<cmd>lua vimux_run()<cr>")
+  keymap("n", "<leader>vr", "<cmd>lua run_cur_buf()<cr>")
   keymap("n", "<leader>vl", "<cmd>VimuxRunLastCommand<cr>")
   keymap("n", "<leader>vc", "<cmd>VimuxCloseRunner<cr>")
   keymap("n", "<leader>vi", "<cmd>VimuxInspectRunner<cr>")
   keymap("n", "<leader>vp", "<cmd>VimuxPromptCommand<cr>")
-  keymap("n", "<leader>vb", [[<cmd>lua vimux_run("bundle exec ruby")<cr>]])
-  keymap("n", "<leader>rs", [[<cmd>VimuxRunCommand("rails s")<cr>]])
+  keymap("n", "<leader>vb", [[<cmd>lua run_cur_buf("bundle exec ruby")<cr>]])
+  keymap("n", "<leader>vs", [[<cmd>VimuxRunCommand("rails s")<cr>]])
 
   -- Settings
   vim.g["VimuxUseNearest"] = 0
