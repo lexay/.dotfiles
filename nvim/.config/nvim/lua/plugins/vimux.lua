acb5bcfad0dced9c8a143951d72ea4dab2f1f147
Aleksei Khmyz <alexcrpts@gmail.com>
2024-10-23 18:06:56 +0300

    (vimux): open and focus runner

--- nvim/.config/nvim/lua/plugins/vimux.lua
+++ nvim/.config/nvim/lua/plugins/vimux.lua
@@ -25,11 +25,17 @@ M.config = function()
     vim.fn.VimuxRunCommand(string.format(" clear; %s %s", cmd, bufname))
   end
 
+  function open_runner()
+    vim.cmd("VimuxOpenRunner")
+    vim.cmd([[call feedkeys("\<C-j>")]])
+  end
+
   -- Keymaps
   keymap("n", "<leader>ee", "<cmd>lua run_cur_buf()<cr>")
   keymap("n", "<leader>el", "<cmd>VimuxRunLastCommand<cr>")
-  keymap("n", "<leader>ex", "<cmd>VimuxCloseRunner<cr>")
   keymap("n", "<leader>ei", "<cmd>VimuxInspectRunner<cr>")
+  keymap("n", "<leader>eo", "<cmd>lua open_runner()<cr>")
+  keymap("n", "<leader>ex", "<cmd>VimuxCloseRunner<cr>")
   keymap("n", "<leader>ep", "<cmd>VimuxPromptCommand<cr>")
   keymap("n", "<leader>eeb", [[<cmd>lua run_cur_buf("bundle exec ruby")<cr>]])
   keymap("n", "<leader>ees", [[<cmd>VimuxRunCommand("rails s")<cr>]])
