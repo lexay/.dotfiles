7797a64a9d72737376337d7811ac89c38bc599ed
Aleksei Khmyz <lexay@tuta.io>
2025-04-19 18:13:03 +0300

    (neovim, telescope): add keymap showing all buffers

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -58,11 +58,12 @@ M.config = function()
     },
   })
 
-  -- Keymaps
+  -- Navigation
   local map = vim.keymap.set
   map("n", "<leader>ff", "<cmd>Telescope find_files<cr>")
   map("n", "<leader>fg", "<cmd>Telescope live_grep<cr>")
   map("n", "<leader>fb", "<cmd>lua require('telescope.builtin').buffers({only_cwd = true, sort_mru = true})<cr>")
+  map("n", "<leader>fB", "<cmd>lua require('telescope.builtin').buffers()<cr>")
   map("n", "<leader>fh", "<cmd>Telescope help_tags<cr>")
   map("n", "<leader>fm", "<cmd>Telescope man_pages<cr>")
   -- LSP
