efb558b47ebd5bd922f9986c54ece375a1000053
Aleksei Khmyz <lexay@tuta.io>
2025-04-19 18:15:54 +0300

    (neovim, vimux): move custom functions out of config
    * Make them module functions. And do not do function defenition nesting
      inside other fucntions ever, that's nonsense!

--- nvim/.config/nvim/lua/plugins/vimux/init.lua
+++ nvim/.config/nvim/lua/plugins/vimux/init.lua
@@ -5,23 +5,6 @@ local M = {
 M.branch = "master"
 
 M.config = function()
-  local function check_cmd()
-    local commands = require("plugins.vimux.filetypes").commands
-
-    return commands[vim.bo.filetype]()
-  end
-
-  function run_cur_buf(cmd)
-    cmd = cmd or check_cmd()
-    local bufname = vim.api.nvim_buf_get_name(0)
-    vim.fn.VimuxRunCommand(string.format("clear; %s %s", cmd, bufname))
-  end
-
-  function open_runner()
-    vim.cmd("VimuxOpenRunner")
-    vim.cmd([[call feedkeys("\<C-j>")]])
-  end
-
   -- Keymaps
   local map = vim.keymap.set
   map("n", "<leader>l", "<cmd>VimuxRunLastCommand<cr>")
@@ -30,6 +13,23 @@ M.config = function()
 
   -- Settings
   vim.g["VimuxUseNearest"] = 0
+end
+
+function M.check_cmd()
+  local commands = require("plugins.vimux.filetypes").commands
+
+  return commands[vim.bo.filetype]()
+end
+
+function M.run_cur_buf(cmd)
+  cmd = cmd or M.check_cmd()
+  local bufname = vim.api.nvim_buf_get_name(0)
+  vim.fn.VimuxRunCommand(string.format("clear; %s %s", cmd, bufname))
+end
+
+function M.open_runner()
+  vim.cmd("VimuxOpenRunner")
+  vim.cmd([[call feedkeys("\<C-j>")]])
 end
 
 return M
